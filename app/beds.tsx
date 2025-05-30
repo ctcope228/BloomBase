d1dc515041f7f7af8cdc52414a739ef8e6d6de8b
ctcope228 <ctcope228@gmail.com>
2025-05-30 19:25:45 -0400

    Tailwind setup

---
+++ app/beds.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const Beds = () => {
+    return (
+        <View>
+            <Text>Beds</Text>
+        </View>
+    );
+};
+
+export default Beds;
+const styles = StyleSheet.create({})
---
+++ app/plants.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Plants = () => {
+    return (
+        <View>
+            <Text>Plants</Text>
+        </View>
+    );
+};
+
+export default Plants;
+const styles = StyleSheet.create({})
